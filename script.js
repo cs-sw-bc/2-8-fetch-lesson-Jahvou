6ae03695278cb4a94714c3cc3c0cbda7c0f7d562
Jahvou <yearwood.jahvou@gmail.com>
2025-10-30 20:17:55 -0400

    fetch lesson

--- script.js
+++ script.js
@@ -14,22 +14,25 @@ const weatherData = {
 
 // ----- Function to update DOM -----
 function displayWeather(data) {
-  document.getElementById("cityName").textContent = "Your city";
-  document.getElementById("temperature").textContent = `8°C`;
-  document.getElementById("feelslike").textContent = `6°C`;
-  document.getElementById("humidity").textContent = `90%`;
-  document.getElementById("weatherDesc").textContent = `Moderate rain`;
-  document.getElementById("weatherIcon").src = "https://cdn.worldweatheronline.com/images/wsymbols01_png_64/wsymbol_0010_heavy_rain_showers.png";
+  document.getElementById("cityName").textContent = data.location.name;
+  document.getElementById("temperature").textContent = data.current.temperature + "°C";
+  document.getElementById("feelslike").textContent = data.current.feelslike + "°C";
+  document.getElementById("humidity").textContent = `${data.current.humidity}%`;
+  document.getElementById("weatherDesc").textContent = data.current.weather_descriptions[0];
+  document.getElementById("weatherIcon").src = data.current.weather_icons[0];
 }
 
 
 // ----- Modern way: Fetch API -----
 async function getWeather() {
-  const city = document.getElementById("cityInput").value || "Mississauga";
+  const city = document.getElementById("cityInput").value || "Montreal";
   const errorMsg = document.getElementById("errorMsg");
   try {
-    displayData={};
-    displayWeather(displayData);
+    fetch(`https://api.weatherstack.com/current?access_key=e92f54a560182c8252ea90569a1b4bc1& query=${city}`)
+    .then(response => response.json()) // Parse JSON response
+    .then(data => displayWeather(data)) // Use the data
+    .catch(error => console.log(error));
+
   } catch (error) {
     console.error(error);
     errorMsg.classList.remove("hidden");
